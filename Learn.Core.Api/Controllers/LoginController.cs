0ba64722171e2832f66c3edf9451436f820aa2cc
lyz-0911 <774056136@qq.com>
2022-08-18 10:59:04 +0800

    新增JWT生成

---
+++ Learn.Core.Api/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using Learn.Core.Extensions.Authorizations.Helpers;
+using Learn.Core.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learn.Core.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LoginController : ControllerBase
+	{
+		/// <summary>
+		/// 获取JWT
+		/// </summary>
+		/// <param name="name">用户名</param>
+		/// <param name="password">密码</param>
+		/// <returns></returns>
+		[HttpGet]
+		[Route("Token")]
+		public async Task<MessageModel<string>> GetJwt(string name, string password)
+		{
+			string jwtStr = string.Empty;
+			bool suc=false;
+
+			//判断用户登录，并读取用户权限
+			//var user = await _sysUserInfoServices.GetUserRoleNameStr(name, MD5Helper.MD5Encrypt32(pass));
+			var user = name;
+			if(user!=null)
+			{
+				TokenModelJwt tokenModel = new TokenModelJwt { Uid = new Guid().ToString(), Role = user };
+
+				jwtStr = JwtHelper.IssueJwt(tokenModel);
+				suc = true;
+			}
+			else
+			{
+				jwtStr = "登录错误！";
+			}
+			return new MessageModel<string>()
+			{
+				success = suc,
+				msg = suc ? "获取成功" : "获取失败",
+				response = jwtStr
+			};
+		}
+	}
+}
---
+++ Learn.Core.Extensions/Authorizations/Helpers/JwtHelper.cs
@@ -0,0 +1,102 @@
+﻿using Learn.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Learn.Core.Extensions.Authorizations.Helpers
+{
+	public class JwtHelper
+	{
+        /// <summary>
+        /// 生成JWT
+        /// </summary>
+        /// <param name="tokenModel"></param>
+        /// <returns></returns>
+		public static string IssueJwt(TokenModelJwt tokenModel)
+		{
+            //读取Jwt配置信息
+            string iss = Appsettings.app(new string[] { "JwtCofing", "Issuer" });//颁发令牌方（服务端）
+            string aud = Appsettings.app(new string[] { "JwtCofing", "Audience" });//令牌使用方（子客户端）
+            string secret = Appsettings.app(new string[] { "JwtCofing", "Secret" });//加密秘钥
+
+            //JWT有效载荷 也就是jwt三部分中的 中间body部分
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Jti,tokenModel.Uid),//用户ID
+                new Claim(JwtRegisteredClaimNames.Iat,new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),//jwt创建时间 
+                new Claim(JwtRegisteredClaimNames.Nbf,new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),//jwt被接受处理之前的时间
+                new Claim(JwtRegisteredClaimNames.Exp,new DateTimeOffset(DateTime.Now.AddSeconds(1000)).ToUnixTimeSeconds().ToString()),//过期时间
+                new Claim(ClaimTypes.Expiration,DateTime.Now.AddSeconds(1000).ToString()),//
+                new Claim(JwtRegisteredClaimNames.Iss,iss),//颁布者
+                new Claim(JwtRegisteredClaimNames.Aud,aud),//使用者
+                //new Claim(ClaimTypes.Role,tokenModel.Role),//权限  下面代码添加多权限
+            };
+            claims.AddRange(tokenModel.Role.Split(',').Select(s=>new Claim(ClaimTypes.Role,s)));//一个用户多角色
+
+            //处理秘钥
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));//SymmetricSecurityKey会对秘钥进行检测，秘钥长度少于16位会报异常
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);//设置秘钥加密方法
+
+            //实例jwt对象
+            var jwt = new JwtSecurityToken(
+                issuer: iss,
+                claims: claims,
+                signingCredentials: creds);
+
+            var jwtHandler = new JwtSecurityTokenHandler();//实例jwt操作类
+            var encodedJwt = jwtHandler.WriteToken(jwt);//生成jwt字符
+
+            return encodedJwt;
+        }
+
+        /// <summary>
+        /// 解析JWT
+        /// </summary>
+        /// <param name="jwtStr"></param>
+        /// <returns></returns>
+        public static TokenModelJwt SerializeJwt(string jwtStr)
+        {
+            var jwtHandler = new JwtSecurityTokenHandler();
+            TokenModelJwt tokenModelJwt = new TokenModelJwt();
+
+            // token校验
+            if (!jwtStr.IsNullOrEmpty() && jwtHandler.CanReadToken(jwtStr))
+            {
+                //解析jwt字符串成对象
+                JwtSecurityToken jwtToken = jwtHandler.ReadJwtToken(jwtStr);
+
+                object role;
+                //获取权限数据
+                jwtToken.Payload.TryGetValue(ClaimTypes.Role, out role);
+
+                tokenModelJwt = new TokenModelJwt
+                {
+                    Uid = jwtToken.Id,
+                    Role = role != null ? role.ToString() : "",
+                };
+            }
+            return tokenModelJwt;
+
+        }
+    }
+	public class TokenModelJwt
+	{
+        /// <summary>
+        /// Id
+        /// </summary>
+        public string Uid { get; set; }
+        /// <summary>
+        /// 角色
+        /// </summary>
+        public string Role { get; set; }
+        /// <summary>
+        /// 职能
+        /// </summary>
+        public string Work { get; set; }
+    }
+}
