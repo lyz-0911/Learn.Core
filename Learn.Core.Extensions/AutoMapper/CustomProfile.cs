35f95c1e32ec737c01113b4e4d48094a5e619f18
lyz-0911 <774056136@qq.com>
2022-09-07 14:51:30 +0800

    新增AtuoMapper实现DTO

---
+++ Learn.Core.Extensions/AutoMapper/CustomProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.Core.Extensions.AutoMapper
+{
+	public class CustomProfile:Profile
+	{
+		/// <summary>
+		/// 配置构造函数，在构造函数里进行数据entity与viewmodel对象的关系映射
+		/// </summary>
+		public CustomProfile()
+		{
+
+		}
+	}
+}
---
+++ Learn.Core.Extensions/ServicesExtensions/AutoMapperSetup.cs
@@ -0,0 +1,21 @@
+﻿using Learn.Core.Extensions.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.Core.Extensions.ServicesExtensions
+{
+	public static class AutoMapperSetup
+	{
+		public static void AddAutoMapperSetup(this IServiceCollection services)
+		{
+			if(services == null) throw new ArgumentNullException(nameof(services));
+
+			services.AddAutoMapper(typeof(AutoMapperConfig));
+			
+		}
+	}
+}
